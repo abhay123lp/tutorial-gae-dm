b1bad31653be0a8cb8653fa734dc89312c0d1d5e
fabio.magnani3 <fabio.magnani3@gmail.com>
2011-11-05 13:33:30 +0000

    Visualizzazione griglia della guestbook. Comunicazione tra client e server.

--- Tutorial-gae-dm/src/it/unibo/client/GreetingServiceAsync.java
+++ Tutorial-gae-dm/src/it/unibo/client/GreetingServiceAsync.java
@@ -1,11 +1,15 @@
 package it.unibo.client;
 
+
+import it.unibo.shared.RecordQuestbook;
+
+import java.util.Vector;
+
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 /**
  * The async counterpart of <code>GreetingService</code>.
  */
 public interface GreetingServiceAsync {
-	void greetServer(String input, AsyncCallback<String> callback)
-			throws IllegalArgumentException;
+	void dataCloud(AsyncCallback<Vector<RecordQuestbook>> callback);
 }
--- Tutorial-gae-dm/src/it/unibo/server/GreetingServiceImpl.java
+++ Tutorial-gae-dm/src/it/unibo/server/GreetingServiceImpl.java
@@ -1,48 +1,52 @@
 package it.unibo.server;
 
 import it.unibo.client.GreetingService;
-import it.unibo.shared.FieldVerifier;
+import it.unibo.shared.RecordQuestbook;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Vector;
+
+import com.google.appengine.api.rdbms.AppEngineDriver;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 /**
  * The server side implementation of the RPC service.
  */
 @SuppressWarnings("serial")
-public class GreetingServiceImpl extends RemoteServiceServlet implements
-		GreetingService {
+public class GreetingServiceImpl extends RemoteServiceServlet implements GreetingService {
 
-	public String greetServer(String input) throws IllegalArgumentException {
-		// Verify that the input is valid. 
-		if (!FieldVerifier.isValidName(input)) {
-			// If the input is not valid, throw an IllegalArgumentException back to
-			// the client.
-			throw new IllegalArgumentException(
-					"Name must be at least 4 characters long");
-		}
+	public Vector<RecordQuestbook> dataCloud() {
+		Vector<RecordQuestbook> listGuestbook = new Vector<RecordQuestbook>();
+		Connection c = null;
+		
+		try {
+			// Imposto la connessione
+	    	DriverManager.registerDriver(new AppEngineDriver());
+	    	// Connessione remoto
+	    	//c = DriverManager.getConnection("jdbc:google:rdbms://tutorial-id:firstinstance/guestbook");
+	    	// Connessione locale
+	    	c = DriverManager.getConnection("jdbc:google:rdbms://localhost:3306/guestbook","root","fabio.magnani3");
+	    	ResultSet rs = c.createStatement().executeQuery("SELECT * FROM entries");
+	    	while (rs.next()){
+	    		listGuestbook.add(new RecordQuestbook(rs.getString("entryID"),rs.getString("guestName"),rs.getString("content")));
+	    	}
+	    } 
+	    catch (SQLException e) {
+	        e.printStackTrace();
+	    } 
+	    finally {
+	    	if (c != null) {
+	    		try {
+	    			c.close();
+	            } 
+	    		catch (SQLException ignore) {
+	            }
+	    	}
+	    }
 
-		String serverInfo = getServletContext().getServerInfo();
-		String userAgent = getThreadLocalRequest().getHeader("User-Agent");
-
-		// Escape data from the client to avoid cross-site script vulnerabilities.
-		input = escapeHtml(input);
-		userAgent = escapeHtml(userAgent);
-
-		return "Hello, " + input + "!<br><br>I am running " + serverInfo
-				+ ".<br><br>It looks like you are using:<br>" + userAgent;
-	}
-
-	/**
-	 * Escape an html string. Escaping data received from the client helps to
-	 * prevent cross-site script vulnerabilities.
-	 * 
-	 * @param html the html string to escape
-	 * @return the escaped string
-	 */
-	private String escapeHtml(String html) {
-		if (html == null) {
-			return null;
-		}
-		return html.replaceAll("&", "&amp;").replaceAll("<", "&lt;")
-				.replaceAll(">", "&gt;");
+		return listGuestbook;
 	}
 }
